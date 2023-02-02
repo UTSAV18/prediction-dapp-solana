1845628651e9708fb6ed515b2a2a8c22ecf88b4f
UTSAV18 <utsav.addy@gmail.com>
2023-02-03 02:03:39 +0530

    first commit

---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const RPC_ENDPOINT = "https://billowing-side-market.solana-devnet.discover.quiknode.pro/0f803bacba11a510c6c404df4875a60cee01932c/";
+
+export const PROGRAM_ID = new PublicKey("8RZmXCAhhT19brJ1Ro6Fwb2xvuAn8Z2Hk9GyFg5Fk97r");
+
+export const MINIMUM_REMAINING_TIME_UNTIL_EXPIRY = 120;
